fb6ee3955f2eb1fdc94ebb5b403cda76b6705d9d
Miguel Beckers <52552717+miguelbeckers@users.noreply.github.com>
2023-04-14 14:24:19 +0100

    text sending

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,33 +3,23 @@ package com.example.myapplication
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var buttonId = R.id.button
-        var botao = this.findViewById<Button>(buttonId)
-
-        var textId = R.id.textView
-        var caixa = this.findViewById<TextView>(textId)
-
-        var valor = 0
-
-        botao.setOnClickListener{
-            valor ++
-            caixa.text = valor.toString()
-        }
     }
 
-    fun listUsers(view: View) {
-        val intent = Intent(this, ListUsers::class.java)
+    fun showText(view: View) {
+        val intent = Intent(this, ShowText::class.java)
         val bundle = Bundle()
-        bundle.putString("hello", "world")
 
+        val textId = R.id.inputText
+        val inputText = this.findViewById<TextInputEditText>(textId)
+
+        bundle.putString("key", inputText.text.toString())
         intent.putExtras(bundle)
         startActivity(intent)
     }
--- app/src/main/java/com/example/myapplication/ShowText.kt
+++ app/src/main/java/com/example/myapplication/ShowText.kt
@@ -2,15 +2,19 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 
-class ListUsers : AppCompatActivity() {
+class ShowText : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_list_users)
-        var value = intent.extras?.getString("hello")
-
+        setContentView(R.layout.activity_show_text)
+        var value = intent.extras?.getString("key")
         var textView = findViewById<TextView>(R.id.textView2)
         textView.text = value
+    }
+
+    public fun goBack(view: View){
+        finish()
     }
 }
